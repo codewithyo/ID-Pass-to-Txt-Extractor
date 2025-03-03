c0a4a16e3911129427712715abb9e3d5462a9df4
codewithyo <102235878+codewithyo@users.noreply.github.com>
2025-03-03 10:05:14 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 21705536
-api_hash = os.environ.get("API_HASH", "c5bb241f6e3ecf33fe68a444e288de2d")
+api_id = 23634056
+api_hash = os.environ.get("API_HASH", "f2debf49c2f57bad88086ecd17cb5df3")
 bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "5591734243,1369808729,5957208798")
+auth_users = os.environ.get("AUTH_USERS", "5082207960,7612404009")
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "5591734243,1369808729,5957208798")
+osowner_users = os.environ.get("OWNER_USERS", "5082207960,7612404009")
 owner_users = [int(num) for num in osowner_users.split(",")]
