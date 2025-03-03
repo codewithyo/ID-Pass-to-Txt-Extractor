ce1107f99a92b7cf5129cf0bb09dc572ef9c8342
codewithyo <102235878+codewithyo@users.noreply.github.com>
2025-03-03 10:17:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id =23634056
 api_hash = "f2debf49c2f57bad88086ecd17cb5df3"
 bot_token = "7565954298:AAECn0qreImuXP9wBaJuJowO9He65czJb84"
-auth_users = "5082207960,7612404009"
+auth_users = "5082207960"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "5082207960"
 owner_users = [int(num) for num in osowner_users.split(",")]
